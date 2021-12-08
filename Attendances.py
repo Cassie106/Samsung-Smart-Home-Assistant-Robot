6fe5c8807b48c2fac1209b79f830d0155bf7cb14
Lu <luy@alphamd.ai>
2021-12-08 00:37:33 -0800

    Changed the camera outlier

--- Attendances.py
+++ Attendances.py
@@ -66,35 +66,35 @@ while True:
     facesCurFrame = face_recognition.face_locations(imgS)
     encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
-    cv2.imshow('Webcam', img)
-    cv2.waitKey(1)
-
     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
         print(faceDis)
         matchIndex = np.argmin(faceDis)
 
+        # draw square
+        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+
+        name = ''
         # find out who
-        if matches[matchIndex]:
+        if matches[matchIndex] and faceDis[matchIndex] <= 0.51:
             name = classNames[matchIndex].upper()
             print('Household ' + name + '. The door is unlocked, welcome home!')
-            markAttendance(name)
         else:
             name = 'Unknown'
             print('Unknown visitor is at the door. ')
-            markAttendance(name)
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 3)
-            markAttendance(name)
+        markAttendance(name)
+        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 3)
+        cv2.imshow('Webcam', img)
+        cv2.waitKey(1)
 
-# cv2.imshow('Webcam', img)
-# cv2.waitKey(1)
-cap.release()
-cv2.destroyAllWindows()
+# cap.release()
+# cv2.destroyAllWindows()
+
+
 
 
 
